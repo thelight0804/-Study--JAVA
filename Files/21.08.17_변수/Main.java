131a5644d38de6ac5aa3b62b9b94bfc8af6f0504
thelight0804 <thelight0804@gmail.com>
2021-08-17 17:30:28 +0900

    variable

---
+++ Files/21.08.17_변수/Main.java
@@ -0,0 +1,11 @@
+//21.08.17 - 변수
+public class Main{
+    public static void main(String[] args){
+        int a=5, b=10, sum=0;
+        sum=a+b;
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        System.out.println("sum = " + sum);
+        //C언어의 변수와 닮은꼴이다
+    }
+}
