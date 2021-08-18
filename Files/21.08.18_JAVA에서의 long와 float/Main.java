982b7f5ed68a2f6762e496cf25c552f0325f822b
thelight0804 <thelight0804@gmail.com>
2021-08-18 18:33:54 +0900

    long and float

---
+++ Files/21.08.18_JAVA에서의 long와 float/Main.java
@@ -0,0 +1,12 @@
+public class Main{
+    public static void main(String[] args){
+        long l = 700000000L; //long의 큰 수를 사용할땐 값 끝에 L을 붙어야한다
+        //long l = 700; (O)
+        float f = 9.8F; //float를 사용할땐 값 끝에 F를 붙어야한다
+        boolean b = true;
+        
+        System.out.println(l);
+        System.out.println(f);
+        System.out.println(b);
+    }
+}
