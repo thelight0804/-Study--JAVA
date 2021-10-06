6e5034aa35908d6b5b2a674a1c62d67ad795190f
unknown <ddoepsh@office.deu.ac.kr>
2021-10-06 20:30:46 +0900

    I hate Netbeans

---
+++ Files/University/java_maven/src/main/java/cse/oop2/ch03/stream/Gender.java
@@ -0,0 +1,12 @@
+package cse.oop2.ch03.stream;
+
+//enum : 열겨형 / 여러 상황을 자동으로 값을 매겨준다.
+
+public enum Gender{
+    FEMALE("female"),
+    MALE("male");
+    
+    private String info;
+    private Gender(String info){this.info = info;}
+    public String toString(){return info;}
+}
--- Files/University/java_maven/src/main/java/cse/oop2/ch03/stream/StreamTestDrive.java
+++ Files/University/java_maven/src/main/java/cse/oop2/ch03/stream/StreamTestDrive.java
@@ -8,6 +8,23 @@ import java.util.stream.Stream;
 public class StreamTestDrive {
 
     public static List<Person> persons = new LinkedList<>();
+    
+    public static void initialize(){
+    Person[] data = {
+        new Person("Linda", 21, Gender.FEMALE),
+        new Person("Oliver", 25, Gender.MALE),
+        new Person("Alice", 27, Gender.FEMALE),
+        new Person("Noah", 19, Gender.MALE),
+        new Person("Abby", 23, Gender.FEMALE),
+        new Person("Dasisy", 25, Gender.FEMALE),
+        new Person("Samuel", 31, Gender.MALE),
+        new Person("Crystal", 31, Gender.FEMALE),
+        new Person("Tadeo", 33, Gender.MALE),
+    };
+    for(Person p : data){
+        persons.add(p);
+    }
+}
 
     public static void main(String[] args) {
         initialize();
@@ -15,40 +32,22 @@ public class StreamTestDrive {
         //남자 수
         long maleCount = persons.stream()
                 .filter(e->e.getGender() == Gender.MALE).count(); //.filter : 뽑아낸다
-        System.out.printf(">>> 남자는 %d명 입니다.%n"), maleCount); //%n : 줄바꿈
+        System.out.printf(">>> 남자는 %d명 입니다.%n", maleCount);
         
         //여자 나이
-        OptionalDouble gemailAverageAge = persons.stream()
+        OptionalDouble femaleAverageAge = persons.stream()
                 .filter(e->e.getGender() == Gender.FEMALE)
                 .mapToInt(Person::getAge)
                 .average(); //정렬
-        if(femaileAverageAge.isPresent()){
+        if(femaleAverageAge.isPresent()){
             System.out.printf(">>> 여자의 평균 나이는 &.2f입니다.%n", femaleAverageAge.getAsDouble());
         }
         
         System.out.println(">>> 20~25살인 여자의 이름을 정렬해서 출력");
         Stream<Person> sp = persons.stream()
                 .filter(e->e.getGender()==Gender.FEMALE)
-                .filter(e->e.getGender() >= 20&&e.getAge() <= 25)
+                .filter(e->e.getAge() >= 20&&e.getAge() <= 25)
                 .sorted();
         sp.forEach(e->System.out.printf("%s%n", e.getName()));
-    }
-}
-
-
-public static void initialize(){
-    Person[] data = {
-        new Person("Linda", 21, Gender.FEMAIL),
-        new Person("Oliver", 25, Gender.MAIL),
-        new Person("Alice", 27, Gender.FEMAIL),
-        new Person("Noah", 19, Gender.MAIL),
-        new Person("Abby", 23, Gender.FEMAIL),
-        new Person("Dasisy", 25, Gender.FEMAIL),
-        new Person("Samuel", 31, Gender.MAIL),
-        new Person("Crystal", 31, Gender.FEMAIL),
-        new Person("Tadeo", 33, Gender.MAIL),
-    };
-    for(Person p : data){
-        persons.add(p);
     }
 }
